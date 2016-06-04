92f1e6c344fb9c61ecbdf0f58e6312fda8e3c2cd
wiewi0r <mpawelski91@gmail.com>
2016-06-04 15:58:55 +0200

    zmiany

---
+++ Kulka3/RecordModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulka3
+{
+    class RecordModel
+    {
+        public string Nick { get; set; }
+
+        public int Score { get; set; }
+
+        public string Localization { get; set; }
+    }
+}
--- Kulka3/ScoreWindow.xaml.cs
+++ Kulka3/ScoreWindow.xaml.cs
@@ -8,15 +8,21 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Diagnostics;
+using System.IO.IsolatedStorage;
 
 namespace Kulka3
 {
     public partial class ScoreWindow : PhoneApplicationPage
     {
+        private List<RecordModel> records;
+
         public ScoreWindow()
         {
             InitializeComponent();
+            records = new List<RecordModel>();
         }
+
+        IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -26,5 +32,25 @@ namespace Kulka3
             if (NavigationContext.QueryString.TryGetValue("msg", out msg))
                 Debug.WriteLine(msg);
         }
+
+        private void ReadRecords()
+        {
+            //zakladam ze wyniki sa od 1 do 10 i kazdy wynik zapisany jest po danym numerkiem
+            for (int i = 1; i != 11; i++)
+            {
+                if (settings.Contains(i.ToString()))
+                {
+                    string nick, localization;
+                    int score;
+                    var setting = IsolatedStorageSettings.ApplicationSettings[i.ToString()] as string;
+                    ParseSetting(setting, out nick, out score, out localization);
+                }
+            }
+        }
+
+        private void ParseSetting(string setting, out string nick, out int score, out string localization)
+        {
+            throw new NotImplementedException();
+        }
+
     }
-}
